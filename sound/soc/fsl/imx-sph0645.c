54f60b20598e763d884f5c5fc2f186472622c594
Hitesh Kasera <hitesh.kasera@sleepiz.com>
2019-04-16 10:36:49 +0530

    changed tdm slot width to 32

--- sound/soc/fsl/imx-sph0645.c
+++ sound/soc/fsl/imx-sph0645.c
@@ -35,7 +35,7 @@ static int imx_sph0645_hw_params(struct snd_pcm_substream *substream,
 		return ret;
 	}
 
-	ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 3, 2, 24);
+	ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0, 3, 2, 32);
 	if (ret) {
 		dev_err(cpu_dai->dev, "failed to set dai tdm slot\n");
 		return ret;
